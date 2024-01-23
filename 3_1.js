cd727f99b7c58de1bc91e3e2e1daa3455af6263b
Elias <eliasmorenodiaz03@gmail.com>
2024-01-23 19:44:38 -0300

    Manejo de arrrays

---
+++ 3_1.js
@@ -0,0 +1,34 @@
+// Crear un array para la lista de compras
+let lista_compras = ["Leche", "Pan", "Huevos"]
+
+
+// Revisar la longitud de la lista
+console.log("Longitud de la lista de compras:", lista_compras.length)
+
+// Lista entera sin cambio
+console.log("Lista de compras desactualizada:", lista_compras)
+
+
+// Actualizar uno de los elementos
+lista_compras[1] = "Cereal" // Actualizando el segundo elemento de la lista
+
+// Mostrar la lista entera en consola
+console.log("Lista de compras actualizada:", lista_compras)
+
+
+//Ejemplo profesor 
+
+// Definicion de variable
+let variable = "naranja"
+
+// Definicion de array
+let raza_perros = ["labrador", "doberman", "caniche", 3, false, null]
+
+//Mostrar lista 
+console.log("Lista de razas:", raza_perros)
+
+// Agregar una raza de perro más a la lista al final 
+raza_perros.push("piter")
+
+// Mostrar la lista actualizada en consola
+console.log("Lista de razas actualizada:", raza_perros)
---
+++ arrays_splice.js
@@ -0,0 +1,37 @@
+// Crear un array
+let array_numbers = [1, 2, 3, 4, 5];
+
+// Array nuevo para concat
+let array_names = ['Juan', 'Carlos', 'Pedro']
+
+// Array para pop
+let array_figures = [10, 20, 30, 40, 50] // Ejemplo de array con números
+
+// Mostrar el array original
+console.log("Array original:", array_numbers)
+
+// Agregar elementos al array en la posición 2
+array_numbers.splice(2, 0, 6, 7)
+console.log("Después de agregar elementos en la posición 2:", array_numbers)
+
+// Eliminar elementos desde la posición 3
+array_numbers.splice(3, 2)
+console.log("Después de eliminar 2 elementos desde la posición 3:", array_numbers)
+
+// Reemplazar elementos en la posición 1
+array_numbers.splice(1, 1, 10, 11)
+console.log("Después de reemplazar elemento en la posición 1:", array_numbers)
+
+// Eliminar elementos desde la posición 0 hasta el final
+// Descomentar en caso de querer probar el array vacío
+// array_numbers.splice(0)
+// console.log("Después de eliminar todos los elementos:", array_numbers)
+
+// Concatenar arrays
+const concat_arrays = array_names.concat(array_numbers)
+console.log('Mostrar concatenación:', concat_arrays)
+
+// Crear un nuevo array para usar .pop()
+let popped_element = array_figures.pop()
+console.log("Elemento eliminado con pop:", popped_element)
+console.log("Array después de pop:", array_figures)
